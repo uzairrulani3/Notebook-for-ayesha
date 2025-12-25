21457998d90eeae8ed0fa700fbdfbc06144d3a91
uzairrulani3 <uzairrulani3@gmail.com>
2025-12-25 22:58:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-const music = document.getElementById("music");
+const pages = document.querySelectorAll(".page");
+let index = 0;
 
+const music = document.getElementById("music");
 document.body.addEventListener("click", () => {
   if (music.paused) music.play();
 }, { once: true });
 
 function openBook() {
-  document.getElementById("cover").classList.add("hidden");
-  document.getElementById("p1").classList.remove("hidden");
+  pages[index].classList.remove("show");
+  pages[index].classList.add("hidden");
+  index++;
+  pages[index].classList.remove("hidden");
+  pages[index].classList.add("show");
 }
 
-function nextPage(a,b){
-  document.getElementById(a).classList.add("hidden");
-  document.getElementById(b).classList.remove("hidden");
-}
-
-function startOver(){
-  location.reload();
-}
-
-const snow = document.querySelector(".snow");
-
-for(let i=0;i<70;i++){
-  const s = document.createElement("span");
-  const size = Math.random()*6+3;
-  s.style.width = size+"px";
-  s.style.height = size+"px";
-  s.style.left = Math.random()*100+"vw";
-  s.style.animationDuration = Math.random()*6+6+"s";
-  s.style.opacity = Math.random();
-  snow.appendChild(s);
+function nextPage() {
+  pages[index].classList.remove("show");
+  pages[index].classList.add("hidden");
+  index++;
+  pages[index].classList.remove("hidden");
+  pages[index].classList.add("show");
 }
